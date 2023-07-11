ab54b045f92d12d8f3dc06cacf45736e3f31150a
Cody Peters <cjosephp15@gmail.com>
2023-07-11 14:41:39 +1000

    Added route guard

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
 
 import LandingView from "@/views/LandingView.vue";
 import DashboardView from "@/views/DashboardView.vue";
@@ -11,18 +12,30 @@ const router = createRouter({
       path: "/",
       name: "landing",
       component: LandingView,
+      meta: { requiresAuth: false },
     },
     {
       path: "/dashboard",
       name: "dashboard",
       component: DashboardView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/callback",
       name: "callback",
       component: CallbackView,
+      meta: { requiresAuth: false },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  if (to.meta.requiresAuth && !authStore.accessToken) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
